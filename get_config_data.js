15f1b31cda26b4f69dd1164eaed61d7fa84e079f
aza <aza@lainventoria.com.ar>
2016-08-07 19:56:36 -0300

    crear devices

--- get_config_data.js
+++ get_config_data.js
@@ -7,13 +7,12 @@ if ( process.argv.length < 4 ) {
   throw Error('No email or password provided, run \n node script.js email password');
 }
 
-let w = new Wehaus();
+let w = new Wehaus({});
 
 w.get_token(process.argv[2] ,process.argv[3]);
 w.get_mac_address();
 
 w.on('ready',() => {
   console.log( "\n\n1) Add the following to your script\n\n  let w = new Wehaus({email: '"+w.config.email+"', token: '"+w.config.token+"', mac: '"+w.config.mac+"'});" );
-  console.log( "\n2) Send us this text to hackers@patagoniclabs.com :\n\n  Please enable my device "+w.config.mac+" on account "+w.config.email+"\n" );
 });
 
--- index.js
+++ index.js
@@ -5,15 +5,57 @@ const exec = require('child_process').exec;
 
 const loop_delay_in_seconds = 30;
 
-let w = new Wehaus({email: '', token: '', mac: ''});
+// Inicializamos la librería
+let w = new Wehaus({});
 
-w.get_devices();
+// Traémos los módulos y dispositivos de nuestra cuenta
+w.get_devices_and_modules();
 
-let sensor;
+let sensor_temp, sensor_hum;
 
+// Cuando ya trajo todo
 w.on('ready',() => {
-  sensor = new SensorDevice(w, 4);
-  loop();
+  console.log('ready');
+  //** Primera vez, creo los elementos en el API **//
+
+  // Si no existe ya, creo el módulo
+  if (!w.exist_module(w.config.mac)) {
+    console.log('creating module');
+    w.create_module(w.config.mac);
+  }
+
+  // Cuando no tenga tareas de creado de modulos pendiente
+  w.on('module-ready', () => {
+    console.log('module ready');
+
+    // Si no existe un device en el endpoint 1 creo un sensor
+    if (!w.exist_device(1)) {
+      console.log('creating device 1');
+      w.create_device('SensorDevice',1);
+    }
+
+    // Si no existe un device en el endpoint 2 creo un sensor
+    if (!w.exist_device(2)) {
+      console.log('creating device 2');
+      w.create_device('SensorDevice',2);
+    }
+
+    // Cuando no tenga tareas de creado de dispositivos pendientes
+    w.on('device-ready', () => {
+      console.log('device ready');
+
+      // Inicializo los sensores
+      sensor_temp = new SensorDevice(w, 1);
+      sensor_hum = new SensorDevice(w, 2);
+
+      loop();
+    });
+
+    w.check_device_status();
+  });
+
+  // Chequea si tiene tareas pendientes 
+  w.check_module_status();
 });
 
 
@@ -23,15 +65,17 @@ function loop() {
 }
 
 function read_values() {
+  // Traigo los datos de los sensores
   exec('python output_sensor_data.py', (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console.error(error);
       return;
     }
 
-    if (typeof sensor != 'undefined') {
-      sensor.emit('data', stdout.split(',')[0]);
-    }
-    
+    temperatura, humedad = stdout.split(',')
+
+    // Emito eventos de datos para los sensores
+    sensor_temp.emit('data', temperatura);
+    sensor_hum.emit('data', humedad);
   });
 }
